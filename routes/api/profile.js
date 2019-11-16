4b3d63b1baa3a2c5fbfbb6947ae83ffe50bc91f9
Elad Yair <eladyair@gmail.com>
2019-11-16 22:43:10 +0200

    Creating a profile authenticated rest api path for current user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+// Mongoose schemas (enabling an access to the mongo db through mongoose and the relavent schema)
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user profile
+// @access  Private (Needs a token to access)
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
