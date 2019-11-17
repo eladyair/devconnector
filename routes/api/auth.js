3a13370f2548e0bb33a6cc979ee4e630c0cdb6ff
Elad Yair <eladyair@gmail.com>
2019-11-17 10:40:51 +0200

    Adding a new profile route for creating and updating a user profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,91 +1,75 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const auth = require("../../middleware/auth");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get("/", auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select("-password");
+        const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send("Server Error");
+        res.status(500).send('Server Error');
     }
 });
 
 // @route   POST api/auth
 // @desc    Authenticate user & get token
 // @access  Public
-router.post(
-    "/",
-    [
-        check("email", "Please include a valid email").isEmail(),
-        check("password", "Password is required").exists()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { email, password } = req.body;
-
-        try {
-            let user = await User.findOne({ email });
-
-            // If user doesn't exist
-            if (!user) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: "Invalid Credentials" }] });
-            }
-
-            // Checking to see if the passwords matched (the one that was entered and the one that the user has)
-            const isMatched = await bcrypt.compare(password, user.password);
-
-            // If passwords don't matched
-            if (!isMatched) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: "Invalid Credentials" }] });
-            }
-
-            // User exist and passwords are matched...
-            // |
-            // V
-            // Creating the payload for jwt using the user id returned from after the save
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-
-            // Creating the token to be send to the client
-            jwt.sign(
-                payload,
-                config.get("jwtToken"),
-                { expiresIn: 3600 },
-                (err, jwtToken) => {
-                    if (err) {
-                        throw err;
-                    }
-                    // sending the token back to the client
-                    res.json({ jwtToken });
-                }
-            );
-        } catch (err) {
-            console.log(err.message);
-            res.status(500).send("Server Error");
-        }
+router.post('/', [check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-);
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        // If user doesn't exist
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        // Checking to see if the passwords matched (the one that was entered and the one that the user has)
+        const isMatched = await bcrypt.compare(password, user.password);
+
+        // If passwords don't matched
+        if (!isMatched) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        // User exist and passwords are matched...
+        // |
+        // V
+        // Creating the payload for jwt using the user id returned from after the save
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        // Creating the token to be send to the client
+        jwt.sign(payload, config.get('jwtToken'), { expiresIn: 360000 }, (err, jwtToken) => {
+            if (err) {
+                throw err;
+            }
+            // sending the token back to the client
+            res.json({ jwtToken });
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 // Mongoose schemas (enabling an access to the mongo db through mongoose and the relavent schema)
 const Profile = require('../../models/Profile');
@@ -19,9 +20,79 @@ router.get('/me', auth, async (req, res) => {
 
         res.json(profile);
     } catch (err) {
-        console.log(err.message);
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/profile
+// @desc    Create or update a user profile
+// @access  Private (Needs a token to access)
+router.post(
+    '/',
+    [
+        // Array of middlewares
+        auth, // the auth middleware
+        [
+            // the express check middleware for incoming request data
+            check('status', 'Status is required')
+                .not()
+                .isEmpty(),
+            check('skills', 'Skills is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { company, website, location, bio, status, githubusername, skills, youtube, facebook, twitter, instagram, linkedin } = req.body;
+
+        // Build profile object
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        // Build social object
+        profileFields.social = {};
+        if (youtube) profileFields.social = youtube;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (instagram) profileFields.social.instagram = instagram;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+
+        try {
+            let profile = await Profile.findOne({ user: req.user.id });
+
+            // If profile exist, update the profile
+            if (profile) {
+                profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+
+                return res.json(profile);
+            }
+
+            // Creating a new profile
+            profile = new Profile(profileFields);
+
+            await profile.save();
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server Error' });
+        }
+    }
+);
 
 module.exports = router;
