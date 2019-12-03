f4ac2bf6576452eaee712418032b9966077e5a0f
Yair <yair@macil-82246.local>
2019-12-03 17:28:01 +0200

    Adding the logic for loggig out a user and changing the navbar links accordingly

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
 
 // Load User
 export const loadUser = () => async dispatch => {
@@ -85,4 +85,11 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+};
+
+// Logout User & Clear Profile
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    });
 };
