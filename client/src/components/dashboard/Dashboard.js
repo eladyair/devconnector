58dee748a98b0a3ecdcc6b0700af73a2d3ebbd9d
Yair <yair@macil-82246.local>
2019-12-05 15:46:00 +0200

    Addign code for getting a user profile on login. Creating a reducer and action for the profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
 
-const Dashboard = props => {
+const Dashboard = ({ getCurrentProfile, profile, auth }) => {
+    // As soon as the dahboard loads we make a call to get the user profile
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
+
     return <div>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
