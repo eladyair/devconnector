6ca5ae09398cdac22b563a7409587697f833b282
Elad Yair <eladyair@gmail.com>
2019-12-08 15:49:40 +0200

    Creating the add education and experience forms components, adding them to the routes in App.js and also adding their funcionality (action, reducer, component)

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile.js';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routhing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -18,6 +20,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+import { addEducation } from './actions/profile';
 
 if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -43,6 +46,8 @@ const App = () => {
                             <PrivateRoute exact path='/dashboard' component={Dashboard} />
                             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+                            <PrivateRoute exact path='/add-experience' component={AddExperience} />
+                            <PrivateRoute exact path='/add-education' component={AddEducation} />
                         </Switch>
                     </section>
                 </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 //Get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -40,6 +40,76 @@ export const createProfile = (formData, history, isEdit = false) => async dispat
         if (!isEdit) {
             history.push('/dashboard');
         }
+    } catch (error) {
+        const errors = error.response.data.errors;
+
+        if (errors) {
+            errors.forEach(err => {
+                dispatch(setAlert(err.msg, 'danger'));
+            });
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statustext, status: error.response.status }
+        });
+    }
+};
+
+// Add Experience
+export const addExperience = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/experience', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience Added', 'success'));
+
+        history.push('/dashboard');
+    } catch (error) {
+        const errors = error.response.data.errors;
+
+        if (errors) {
+            errors.forEach(err => {
+                dispatch(setAlert(err.msg, 'danger'));
+            });
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statustext, status: error.response.status }
+        });
+    }
+};
+
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/education', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education Added', 'success'));
+
+        history.push('/dashboard');
     } catch (error) {
         const errors = error.response.data.errors;
 
