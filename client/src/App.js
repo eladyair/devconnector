a1d9ae59594b96025e96f6e31cbc4d1124da24e2
Elad Yair <eladyair@gmail.com>
2019-12-08 12:19:38 +0200

    Creating the create profile component and its functionality and adding a route for it in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Components
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routhing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -38,6 +40,7 @@ const App = () => {
                             <Route exact path='/register' component={Register} />
                             <Route exact path='/login' component={Login} />
                             <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                         </Switch>
                     </section>
                 </Fragment>
