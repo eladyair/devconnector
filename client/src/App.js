84494133353b067d7edc5e9ac06d4daad756ba99
Yair <yair@macil-82246.local>
2019-12-09 16:26:11 +0200

    Creating the Profile component and starting its functionality

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import EditProfile from './components/profile-forms/EditProfile.js';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -38,18 +39,19 @@ const App = () => {
             <Router>
                 <Fragment>
                     <Navbar />
-                    <Route exact path='/' component={Landing} />
-                    <section className='container'>
+                    <Route exact path="/" component={Landing} />
+                    <section className="container">
                         <Alert />
                         <Switch>
-                            <Route exact path='/register' component={Register} />
-                            <Route exact path='/login' component={Login} />
-                            <Route exact path='/profiles' component={Profiles} />
-                            <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
-                            <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-                            <PrivateRoute exact path='/add-experience' component={AddExperience} />
-                            <PrivateRoute exact path='/add-education' component={AddEducation} />
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/profiles" component={Profiles} />
+                            <Route exact path="/profile/:id" component={Profile} />
+                            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                            <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                            <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                            <PrivateRoute exact path="/add-education" component={AddEducation} />
                         </Switch>
                     </section>
                 </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -41,7 +41,7 @@ export const getProfiles = () => async dispatch => {
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
     try {
-        const res = await axios(`/api/profile/user${userId}`);
+        const res = await axios(`/api/profile/user/${userId}`);
 
         dispatch({
             type: GET_PROFILE,
